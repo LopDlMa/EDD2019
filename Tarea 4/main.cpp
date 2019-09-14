efa6048b39481febb07305faab9fefa1ac81804a
LopDlMa <53009107+LopDlMa@users.noreply.github.com>
2019-09-14 00:00:04 -0600

    Add files via upload

---
+++ Tarea 4/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "cabecera_dias.h"
+#include "listaAct.h"
+#include "cabecera_horario.h"
+
+using namespace std;
+
+int main()
+{
+   /*cabecera_dias a ;
+   a.insertarAct("Lunes","correr");
+   a.recorrerv2();
+
+*/
+    cabecera_horario a;
+
+    return 0;
+}
